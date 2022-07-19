0ac276ba0de2c30e1450a83db2e2a15b9fe1d215
Archivopdf <toni.diaz.gar77@gmail.com>
2022-07-19 23:23:44 +0200

    fix

--- Person.js
+++ Person.js
@@ -19,24 +19,28 @@ class Person {
     }
     printAll() {
 
+        for (let properties in Person) {
 
+            console.log(properties + " - " + Person[properties]);
 
-        // for (let i = 0; i < ; i++) {
-
-
+        }
 
     }
+
     printHobbies() {
 
         console.log(this.hobbies);
+
     }
-
-
 }
 
 
 
+
+
+
 let persona = new Person(179, 89);
+console.log(persona.printAll());
 //Reto3
 
 persona.yearOfBith = 2022 - persona.age;
